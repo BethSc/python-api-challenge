d74c63bb69b59845b2b40b324cbc73e6b2f7ada3
Beth Scott <bethchapmanscott@gmail.com>
2021-03-25 14:31:57 +1100

    added updated file

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key="9358ecdef3ddd0afd68b3a47d57e0a3f"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key="AIzaSyBf2Z3J5r3RfaM-L4g9kZHIucxNbsCXF7M"
